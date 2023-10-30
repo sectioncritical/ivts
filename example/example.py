a222a4fd6d992ad85e150999e099ffc0eea35229
Joe Kroesche <joe@kroesche.org>
2023-10-30 10:21:25 -0500

    Modify example to set config and store result

--- example/example.py
+++ example/example.py
@@ -25,35 +25,42 @@ bus = can.Bus(interface="socketcan", channel="can0")
 #  arbid, msgbytes = enc.some_method()
 
 # get some info first
-send_msg(bus, enc.get_serial_num())
-send_msg(bus, enc.get_version())
-send_msg(bus, enc.get_device_id())
+#send_msg(bus, enc.get_serial_num())
+#send_msg(bus, enc.get_version())
+#send_msg(bus, enc.get_device_id())
 
 # first the IVTS has to be stopped before config can be changed
-encoded = enc.set_run_mode(False)  # stops immediate run mode
-send_msg(bus, encoded)
+send_msg(bus, enc.set_run_mode(False))  # stops immediate run mode
 
 # create config message to cause temp reading to generate every 100 ms
-encoded = enc.set_config(ivts.MeasureType.T, ivts.TriggerMode.CYCLIC, 100)
-send_msg(bus, encoded)
+send_msg(bus, enc.set_config(ivts.MeasureType.T, ivts.TriggerMode.CYCLIC, 311))
+
+send_msg(bus, enc.set_config(ivts.MeasureType.I, ivts.TriggerMode.CYCLIC, 7))
+send_msg(bus, enc.set_config(ivts.MeasureType.U1, ivts.TriggerMode.CYCLIC, 7))
 
 # turn off the U2 and U3 reporting to reduce clutter on the output
-encoded = enc.set_config(ivts.MeasureType.U2, ivts.TriggerMode.DISABLED, 60)
-send_msg(bus, encoded)
-encoded = enc.set_config(ivts.MeasureType.U3, ivts.TriggerMode.DISABLED, 60)
-send_msg(bus, encoded)
+send_msg(bus, enc.set_config(ivts.MeasureType.U2, ivts.TriggerMode.DISABLED, 60))
+send_msg(bus, enc.set_config(ivts.MeasureType.U3, ivts.TriggerMode.DISABLED, 60))
+
+send_msg(bus, enc.set_config(ivts.MeasureType.As, ivts.TriggerMode.CYCLIC, 499))
+send_msg(bus, enc.set_config(ivts.MeasureType.W, ivts.TriggerMode.CYCLIC, 47))
+send_msg(bus, enc.set_config(ivts.MeasureType.Wh, ivts.TriggerMode.CYCLIC, 991))
 
 # reset the log data
-send_msg(bus, enc.reset_logdata(ivts.LogItem.As_TOTAL, 18022594))
+#send_msg(bus, enc.reset_logdata(ivts.LogItem.As_TOTAL, 18022594))
+
+# store the config
+send_msg(bus, enc.store())
+time.sleep(2)
 
 # set IVTS run mode on
 encoded = enc.set_run_mode(True)  # starts immediate run mode
 send_msg(bus, encoded)
 
 # request total A-h (As)
-send_msg(bus, enc.get_logdata_item(ivts.LogItem.As_TOTAL, is_persistent=False))
+#send_msg(bus, enc.get_logdata_item(ivts.LogItem.As_TOTAL, is_persistent=False))
 
 # request max U1
-send_msg(bus, enc.get_logdata_item(ivts.LogItem.U1_MAX))
+#send_msg(bus, enc.get_logdata_item(ivts.LogItem.U1_MAX))
 
 bus.shutdown()
