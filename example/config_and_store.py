076aaead8d1adf09ba2f0e9dfb7fdba7be5168e5
Joe Kroesche <joe@kroesche.org>
2023-10-30 10:43:22 -0500

    Clean up config example, add explanatory comments

--- example/config_and_store.py
+++ example/config_and_store.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-# This example shows how to encode some messages and send to IVTS.
-# It does not show how to decode messages.
+# This example shows how to configure the IVTS and store the configuragtion
+# so that it will be retained after a power cycle.
 
 import time
 import ivts
 import can
 
+# A function to roll up all the steps for sending a CAN message.
+#  * canbus - an opened can.Bus
+#  * encresult - the result from most ivts.Encoder methods)
+#
 def send_msg(canbus, encresult):
     arbid = encresult[0]
     msgbytes = encresult[1]
@@ -23,44 +27,35 @@ bus = can.Bus(interface="socketcan", channel="can0")
 # most of the encoder methods return a tuple of arbitration ID
 # and CAN message payload. So you can call them with:
 #  arbid, msgbytes = enc.some_method()
-
-# get some info first
-#send_msg(bus, enc.get_serial_num())
-#send_msg(bus, enc.get_version())
-#send_msg(bus, enc.get_device_id())
+#
+# Using the send_msg() function, this can be simplified to:
+#  send_msg(bus, enc.some_method())
+#
+# Which will encode the desired CAN message and pass it directly to send_msg()
+# for transmitting on the CAN bus
 
 # first the IVTS has to be stopped before config can be changed
 send_msg(bus, enc.set_run_mode(False))  # stops immediate run mode
 
-# create config message to cause temp reading to generate every 100 ms
+# send messages to configure each of the cyclic values to our desired rate
 send_msg(bus, enc.set_config(ivts.MeasureType.T, ivts.TriggerMode.CYCLIC, 311))
-
 send_msg(bus, enc.set_config(ivts.MeasureType.I, ivts.TriggerMode.CYCLIC, 7))
 send_msg(bus, enc.set_config(ivts.MeasureType.U1, ivts.TriggerMode.CYCLIC, 7))
-
-# turn off the U2 and U3 reporting to reduce clutter on the output
-send_msg(bus, enc.set_config(ivts.MeasureType.U2, ivts.TriggerMode.DISABLED, 60))
-send_msg(bus, enc.set_config(ivts.MeasureType.U3, ivts.TriggerMode.DISABLED, 60))
-
 send_msg(bus, enc.set_config(ivts.MeasureType.As, ivts.TriggerMode.CYCLIC, 499))
 send_msg(bus, enc.set_config(ivts.MeasureType.W, ivts.TriggerMode.CYCLIC, 47))
 send_msg(bus, enc.set_config(ivts.MeasureType.Wh, ivts.TriggerMode.CYCLIC, 991))
 
-# reset the log data
-#send_msg(bus, enc.reset_logdata(ivts.LogItem.As_TOTAL, 18022594))
+# turn off the U2 and U3 reporting to reduce clutter on the output
+# note: the time argument must have some non-zero value to avoid message error
+send_msg(bus, enc.set_config(ivts.MeasureType.U2, ivts.TriggerMode.DISABLED, 60))
+send_msg(bus, enc.set_config(ivts.MeasureType.U3, ivts.TriggerMode.DISABLED, 60))
 
 # store the config
 send_msg(bus, enc.store())
-time.sleep(2)
+time.sleep(2)  # give it plenty of time to complete the store operation
 
 # set IVTS run mode on
 encoded = enc.set_run_mode(True)  # starts immediate run mode
 send_msg(bus, encoded)
-
-# request total A-h (As)
-#send_msg(bus, enc.get_logdata_item(ivts.LogItem.As_TOTAL, is_persistent=False))
-
-# request max U1
-#send_msg(bus, enc.get_logdata_item(ivts.LogItem.U1_MAX))
 
 bus.shutdown()
